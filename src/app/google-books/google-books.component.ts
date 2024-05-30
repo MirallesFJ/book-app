4ec0d0a74fee7abc7e755f19c08fb7e7fdaaaf48
Miralles <jorge.miralles@itq.de>
2024-05-30 18:21:39 +0200

    getting books from google books api

--- src/app/google-books/google-books.component.ts
+++ src/app/google-books/google-books.component.ts
@@ -1,25 +1,34 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-google-books',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './google-books.component.html',
   styleUrls: ['./google-books.component.css'],
 })
 export class GoogleBooksComponent {
   searchTerm = '';
   data: any;
+  key = 'AIzaSyAFeoKJviwitXqRmNO7bAR4zNX49xB_CIU';
 
   constructor(private http: HttpClient) {}
 
-  searchBook() {
+  searchBook(searchTerm: string) {
     this.http
       .get<any>(
-        `https://www.googleapis.com/books/v1/volumes?q=${this.searchTerm}&langRestrict=en&orderBy=relevance&maxResults=40`
+        'https://www.googleapis.com/books/v1/volumes?q=' +
+          searchTerm +
+          '&langRestrict=en' +
+          '&orderBy=relevance' +
+          '&key=' +
+          this.key +
+          '&maxResults=40'
       )
       .subscribe({
         next: (res) => {
-          console.log(res.items);
           this.data = res.items;
         },
         error: (err) => console.log(err),
